6df9326a553c3ab244a3bc8cb9ac21b185a844fe
tekintian <tekintian@gmail.com>
2017-07-26 22:17:20 +0800

    lnmp mulity  php version

---
+++ README.md
@@ -0,0 +1,133 @@
+Mulit php version supported  by TekinTian
+
+This script is written using the shell, in order to quickly deploy `LEMP`/`LAMP`/`LNMP`/`LNMPA`(Linux, Nginx/Tengine/OpenResty, MySQL in a production environment/MariaDB/Percona, PHP), applicable to CentOS 5~7(including redhat), Debian 6~8, Ubuntu 12~16 of 32 and 64. 
+
+Script properties:
+- Continually updated
+- Source compiler installation, most stable source is the latest version, and download from the official site
+- Some security optimization
+- Providing a plurality of database versions (MySQL-5.7, MySQL-5.6, MySQL-5.5, MariaDB-10.1, MariaDB-10.0, MariaDB-5.5, Percona-5.7, Percona-5.6, Percona-5.5, AliSQL-5.6)
+- Providing multiple PHP versions (PHP-7.1, PHP-7.0, PHP-5.6, PHP-5.5, PHP-5.4, PHP-5.3)
+- Provide Nginx, Tengine, OpenResty
+- Providing a plurality of Apache version (Apache-2.4, Apache-2.2)
+- According to their needs to install PHP Cache Accelerator provides ZendOPcache, xcache, apcu, eAccelerator. And php encryption and decryption tool ionCube, ZendGuardLoader
+- Installation Pureftpd, phpMyAdmin according to their needs
+- Install memcached, redis according to their needs
+- Jemalloc optimize MySQL, Nginx
+- Providing add a virtual host script, include Let's Encrypt SSL certificate
+- Provide Nginx/Tengine/OpenResty, MySQL/MariaDB/Percona, PHP, Redis, Memcached, phpMyAdmin upgrade script
+- Provide local backup and remote backup (rsync between servers) script
+- Provided under HHVM install CentOS 6,7
+
+## How to use
+
+If your server system: CentOS/Redhat (Do not enter "//" and "// subsequent sentence)
+```bash
+yum -y install wget screen python   // For CentOS / Redhat
+wget http://mirrors.linuxeye.com/lnmp-full.tar.gz   // Contains the source code
+tar xzf lnmp-full.tar.gz
+cd lnmp    // If you need to modify the directory (installation, data storage, Nginx logs), modify options.conf file
+screen -S lnmp    // if network interruption, you can execute the command `screen -r lnmp` reconnect install window
+./install.sh   // Do not sh install.sh or bash install.sh such execution
+```
+If your server system: Debian/Ubuntu (Do not enter "//" and "// subsequent sentence)
+```bash
+apt-get -y install wget screen python   // For Debian / Ubuntu
+wget http://mirrors.linuxeye.com/lnmp-full.tar.gz   // Contains the source code
+tar xzf lnmp-full.tar.gz
+cd lnmp    // If you need to modify the directory (installation, data storage, Nginx logs), modify options.conf file
+screen -S lnmp    // if network interruption, you can execute the command `screen -r lnmp` reconnect install window
+./install.sh   // Do not sh install.sh or bash install.sh such execution
+```
+
+## How to add Extensions
+
+```bash
+cd ~/lnmp    // Must enter the directory execution under lnmp
+./addons.sh    // Do not sh addons.sh or bash addons.sh such execution
+```
+
+## How to add a virtual host
+
+```bash
+cd ~/lnmp    // Must enter the directory execution under lnmp
+./vhost.sh    // Do not sh vhost.sh or bash vhost.sh such execution
+```
+
+## How to delete a virtual host
+
+```bash
+cd ~/lnmp
+./vhost.sh del
+```
+
+## How to add FTP virtual user 
+
+```bash
+cd ~/lnmp
+./pureftpd_vhost.sh
+```
+
+## How to backup
+
+```bash
+cd ~/lnmp
+./backup_setup.sh    // Backup parameters 
+./backup.sh    // Perform the backup immediately 
+crontab -l    // Can be added to scheduled tasks, such as automatic backups every day 1:00 
+  0 1 * * * cd ~/lnmp;./backup.sh  > /dev/null 2>&1 &
+```
+
+## How to manage service
+
+Nginx/Tengine/OpenResty:
+```bash
+service nginx {start|stop|status|restart|reload|configtest}
+```
+MySQL/MariaDB/Percona:
+```bash
+service mysqld {start|stop|restart|reload|status}
+```
+PHP: PHP VERSION: 53  54 55 56 70 71
+```bash
+service php56-fpm {start|stop|restart|reload|status}
+```
+HHVM:
+```bash
+service supervisord {start|stop|status|restart|reload}
+```
+Apache:
+```bash
+service httpd {start|restart|stop}
+```
+Pure-Ftpd:
+```bash
+service pureftpd {start|stop|restart|status}
+```
+Redis:
+```bash
+service redis-server {start|stop|status|restart|reload}
+```
+Memcached:
+```bash
+service memcached {start|stop|status|restart|reload}
+```
+
+## How to upgrade 
+
+```bash
+./upgrade.sh
+```
+
+## How to uninstall 
+
+```bash
+./uninstall.sh
+```
+
+## Installation
+
+Follow the instructions in [Wiki Installation page](https://github.com/tekintian/lnmp/wiki/Installation)<br />
+
+For feedback, questions, and to follow the progress of the project (Chinese): <br />
+[lnmp最新源码一键安装脚本](https://bitbucket.org/tekintian/lnmp)<br />
---
+++ backup.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+# Author:  yeho <lj2007331 AT gmail.com>
+# BLOG:  https://blog.linuxeye.com
+#
+# Notes: OneinStack for CentOS/RadHat 5+ Debian 6+ and Ubuntu 12+
+#
+# Project home page:
+#       https://oneinstack.com
+#       https://github.com/lj2007331/oneinstack
+
+# Check if user is root
+[ $(id -u) != "0" ] && { echo "${CFAILURE}Error: You must be root to run this script${CEND}"; exit 1; }
+
+pushd tools > /dev/null
+. ../options.conf
+
+DB_Local_BK() {
+  for D in `echo $db_name | tr ',' ' '`
+  do
+    ./db_bk.sh $D
+  done
+}
+
+DB_Remote_BK() {
+  for D in `echo $db_name | tr ',' ' '`
+  do
+    ./db_bk.sh $D
+    DB_GREP="DB_${D}_`date +%Y`"
+    DB_FILE=`ls -lrt $backup_dir | grep ${DB_GREP} | tail -1 | awk '{print $NF}'`
+    echo "file:::$backup_dir/$DB_FILE $backup_dir push" >> config_bakcup.txt
+    echo "com:::[ -e "$backup_dir/$DB_FILE" ] && rm -rf $backup_dir/DB_${D}_$(date +%Y%m%d --date="$expired_days days ago")_*.tgz" >> config_bakcup.txt
+  done
+}
+
+DB_COS_BK() {
+  for D in `echo $db_name | tr ',' ' '`
+  do
+    ./db_bk.sh $D
+    DB_GREP="DB_${D}_`date +%Y`"
+    DB_FILE=`ls -lrt $backup_dir | grep ${DB_GREP} | tail -1 | awk '{print $NF}'`
+    ${python_install_dir}/bin/python ./coscmd put $backup_dir/$DB_FILE /`date +%F`/$DB_FILE
+    [ $? -eq 0 ] && ${python_install_dir}/bin/python ./coscmd rm /`date +%F --date="$expired_days days ago"`/ > /dev/null 2>&1
+  done
+}
+
+WEB_Local_BK() {
+  for W in `echo $website_name | tr ',' ' '`
+  do
+    ./website_bk.sh $W
+  done
+}
+
+WEB_Remote_BK() {
+  for W in `echo $website_name | tr ',' ' '`
+  do
+    if [ `du -sm "$wwwroot_dir/$WebSite" | awk '{print $1}'` -lt 1024 ];then
+      ./website_bk.sh $W
+      Web_GREP="Web_${W}_`date +%Y`"
+      Web_FILE=`ls -lrt $backup_dir | grep ${Web_GREP} | tail -1 | awk '{print $NF}'`
+      echo "file:::$backup_dir/$Web_FILE $backup_dir push" >> config_bakcup.txt
+      echo "com:::[ -e "$backup_dir/$Web_FILE" ] && rm -rf $backup_dir/Web_${W}_$(date +%Y%m%d --date="$expired_days days ago")_*.tgz" >> config_bakcup.txt
+    else
+      echo "file:::$wwwroot_dir/$W $backup_dir push" >> config_bakcup.txt
+    fi
+  done
+}
+
+WEB_COS_BK() {
+  for W in `echo $website_name | tr ',' ' '`
+  do
+    [ ! -e "$wwwroot_dir/$WebSite" ] && { echo "[$wwwroot_dir/$WebSite] not exist"; break; }
+    PUSH_FILE="$backup_dir/Web_${W}_$(date +%Y%m%d_%H).tgz"
+    if [ ! -e "$PUSH_FILE" ]; then
+      pushd $wwwroot_dir
+      tar czf $PUSH_FILE ./$W
+      popd
+    fi
+    ${python_install_dir}/bin/python ./coscmd put $PUSH_FILE /`date +%F`/Web_${W}_$(date +%Y%m%d_%H).tgz
+    [ $? -eq 0 ] && { [ -e "$PUSH_FILE" ] && rm -rf $PUSH_FILE; ${python_install_dir}/bin/python ./coscmd rm /`date +%F --date="$expired_days days ago"`/ > /dev/null 2>&1; }
+  done
+}
+
+if [ "$backup_destination" == 'local' ]; then
+  [ -n "`echo $backup_content | grep -ow db`" ] && DB_Local_BK
+  [ -n "`echo $backup_content | grep -ow web`" ] && WEB_Local_BK
+elif [ "$backup_destination" == 'remote' ]; then
+  echo "com:::[ ! -e "$backup_dir" ] && mkdir -p $backup_dir" > config_bakcup.txt
+  [ -n "`echo $backup_content | grep -ow db`" ] && DB_Remote_BK
+  [ -n "`echo $backup_content | grep -ow web`" ] && WEB_Remote_BK
+  ./mabs.sh -c config_bakcup.txt -T -1 | tee mabs.log
+elif [ "$backup_destination" == 'cos' ]; then
+  [ -n "`echo $backup_content | grep -ow db`" ] && DB_COS_BK
+  [ -n "`echo $backup_content | grep -ow web`" ] && WEB_COS_BK
+elif [ "$backup_destination" == 'local,remote' ]; then
+  echo "com:::[ ! -e "$backup_dir" ] && mkdir -p $backup_dir" > config_bakcup.txt
+  [ -n "`echo $backup_content | grep -ow db`" ] && DB_Local_BK
+  [ -n "`echo $backup_content | grep -ow web`" ] && WEB_Local_BK
+  [ -n "`echo $backup_content | grep -ow db`" ] && DB_Remote_BK
+  [ -n "`echo $backup_content | grep -ow web`" ] && WEB_Remote_BK
+  ./mabs.sh -c config_bakcup.txt -T -1 | tee mabs.log	
+elif [ "$backup_destination" == 'local,cos' ]; then
+  [ -n "`echo $backup_content | grep -ow db`" ] && DB_Local_BK
+  [ -n "`echo $backup_content | grep -ow db`" ] && DB_COS_BK
+  [ -n "`echo $backup_content | grep -ow web`" ] && WEB_Local_BK
+  [ -n "`echo $backup_content | grep -ow web`" ] && WEB_COS_BK
+elif [ "$backup_destination" == 'remote,cos' ]; then
+  echo "com:::[ ! -e "$backup_dir" ] && mkdir -p $backup_dir" > config_bakcup.txt
+  [ -n "`echo $backup_content | grep -ow db`" ] && DB_Remote_BK 
+  [ -n "`echo $backup_content | grep -ow db`" ] && DB_COS_BK
+  [ -n "`echo $backup_content | grep -ow web`" ] && WEB_Remote_BK 
+  [ -n "`echo $backup_content | grep -ow web`" ] && WEB_COS_BK
+  ./mabs.sh -c config_bakcup.txt -T -1 | tee mabs.log	
+fi
