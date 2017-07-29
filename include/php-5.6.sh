ad1be1edea9a33410a839128215c9e0109e9a7ef
tekintian <tekintian@gmail.com>
2017-07-29 20:23:13 +0800

    将PHP版本的安装目录直接固定到对应 版本上面

--- include/php-5.6.sh
+++ include/php-5.6.sh
@@ -10,7 +10,9 @@
 
 Install_PHP56() {
   pushd ${oneinstack_dir}/src
+  
   php_install_dir=/usr/local/php56
+
   tar xzf libiconv-$libiconv_version.tar.gz
   patch -d libiconv-$libiconv_version -p0 < libiconv-glibc-2.16.patch
   pushd libiconv-$libiconv_version
